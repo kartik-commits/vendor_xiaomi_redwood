9659bb7f807e286f7224bdb62f372f9112b51401
kartik-commits <kartiklashkare@gmail.com>
2025-07-01 10:56:23 +0000

    redwood: Decommonize citsensorservice

--- redwood-vendor.mk
+++ redwood-vendor.mk
@@ -105,6 +105,7 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/redwood/proprietary/vendor/etc/init/vendor.qti.qspmhal@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.qti.qspmhal@1.0-service.rc \
     vendor/xiaomi/redwood/proprietary/vendor/etc/init/vendor.sensors.qti.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.sensors.qti.rc \
     vendor/xiaomi/redwood/proprietary/vendor/etc/init/vendor.sensors.sscrpcd.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.sensors.sscrpcd.rc \
+    vendor/xiaomi/redwood/proprietary/vendor/etc/init/vendor.xiaomi.hardware.citsensorservice@1.1-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.xiaomi.hardware.citsensorservice@1.1-service.rc \
     vendor/xiaomi/redwood/proprietary/vendor/etc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
     vendor/xiaomi/redwood/proprietary/vendor/etc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
     vendor/xiaomi/redwood/proprietary/vendor/libnfc-nxp_RF.conf:$(TARGET_COPY_OUT_VENDOR)/libnfc-nxp_RF.conf \
@@ -514,6 +515,7 @@ PRODUCT_PACKAGES += \
     camera.xiaomi \
     com.qti.chi.override \
     fingerprint.goodix.default \
+    vendor.xiaomi.hardware.citsensorservice@1.1-impl \
     lib3rdpartcontext \
     libAF \
     libI420colorconvert \
@@ -650,6 +652,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.qspmhal@1.0 \
     vendor.xiaomi.hardware.campostproc@1.0-service-impl \
     vendor.xiaomi.hardware.campostproc@1.0 \
+    vendor.xiaomi.hardware.citsensorservice@1.0 \
+    vendor.xiaomi.hardware.citsensorservice@1.1 \
     vendor.xiaomi.hardware.fx.tunnel@1.0 \
     bm2n06 \
     bm2n08 \
@@ -697,9 +701,11 @@ PRODUCT_PACKAGES += \
     misound_res_headphone \
     misound_res_spk \
     android.hardware.neuralnetworks@1.3-service-qti.xml \
+    vendor.xiaomi.hardware.citsensorservice@1.1-service.xml \
     batterysecret \
     hvdcp_opti \
     android.hardware.neuralnetworks@1.3-service-qti \
+    vendor.xiaomi.hardware.citsensorservice@1.1-service \
     init.qcom.sensors \
     init.qti.chg_policy \
     sensors.qti \
